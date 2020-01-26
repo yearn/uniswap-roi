27f6996aac2aced2b43cd3b1a87f8d535f9abddf
Andre Cronje <andre@fantom.foundation>
2020-01-26 11:22:03 +0200

    uniswap ROI setup
    Signed-off-by: Andre Cronje <andre@fantom.foundation>

---
+++ contracts/IUniswapAPR.sol
@@ -0,0 +1,61 @@
+
+
+interface IUniswapAPR {
+
+    function getCDAIUniROI() public view returns (uint256, uint256);
+    function getCBATUniROI() public view returns (uint256, uint256);
+    function getCETHUniROI() public view returns (uint256, uint256);
+    function getCREPUniROI() public view returns (uint256, uint256);
+    function getCSAIUniROI() public view returns (uint256, uint256);
+    function getCUSDCUniROI() public view returns (uint256, uint256);
+    function getCWBTCUniROI() public view returns (uint256, uint256);
+    function getCZRXUniROI() public view returns (uint256, uint256);
+
+
+    function getIZRXUniROI() public view returns (uint256, uint256);
+    function getIREPUniROI() public view returns (uint256, uint256);
+    function getIKNCUniROI() public view returns (uint256, uint256);
+    function getIWBTCUniROI() public view returns (uint256, uint256);
+    function getIUSDCUniROI() public view returns (uint256, uint256);
+    function getIETHUniROI() public view returns (uint256, uint256);
+    function getISAIUniROI() public view returns (uint256, uint256);
+    function getIDAIUniROI() public view returns (uint256, uint256);
+    function getILINKUniROI() public view returns (uint256, uint256);
+    function getISUSDUniROI() public view returns (uint256, uint256);
+
+    function getADAIUniROI() public view returns (uint256, uint256);
+    function getATUSDUniROI() public view returns (uint256, uint256);
+    function getAUSDCUniROI() public view returns (uint256, uint256);
+    function getAUSDTUniROI() public view returns (uint256, uint256);
+    function getASUSDUniROI() public view returns (uint256, uint256);
+    function getALENDUniROI() public view returns (uint256, uint256);
+    function getABATUniROI() public view returns (uint256, uint256);
+    function getAETHUniROI() public view returns (uint256, uint256);
+    function getALINKUniROI() public view returns (uint256, uint256);
+    function getAKNCUniROI() public view returns (uint256, uint256);
+    function getAREPUniROI() public view returns (uint256, uint256);
+    function getAMKRUniROI() public view returns (uint256, uint256);
+    function getAMANAUniROI() public view returns (uint256, uint256);
+    function getAZRXUniROI() public view returns (uint256, uint256);
+    function getASNXUniROI() public view returns (uint256, uint256);
+    function getAWBTCUniROI() public view returns (uint256, uint256);
+
+    function getDAIUniROI() public view returns (uint256, uint256);
+    function getTUSDUniROI() public view returns (uint256, uint256);
+    function getUSDCUniROI() public view returns (uint256, uint256);
+    function getUSDTUniROI() public view returns (uint256, uint256);
+    function getSUSDUniROI() public view returns (uint256, uint256);
+    function getLENDUniROI() public view returns (uint256, uint256);
+    function getBATUniROI() public view returns (uint256, uint256);
+    function getETHUniROI() public view returns (uint256, uint256);
+    function getLINKUniROI() public view returns (uint256, uint256);
+    function getKNCUniROI() public view returns (uint256, uint256);
+    function getREPUniROI() public view returns (uint256, uint256);
+    function getMKRUniROI() public view returns (uint256, uint256);
+    function getMANAUniROI() public view returns (uint256, uint256);
+    function getZRXUniROI() public view returns (uint256, uint256);
+    function getSNXUniROI() public view returns (uint256, uint256);
+    function getWBTCUniROI() public view returns (uint256, uint256);
+
+    function calcUniswapROI(address token) public view returns (uint256, uint256);
+}
